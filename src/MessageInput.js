cded1da670d158203219e957b5658ccbf909540f
Robert Keller <rkiv@172-29-214-134.dynapool.nyu.edu>
2019-09-28 19:06:18 -0400

    Creating better input box

--- src/MessageInput.js
+++ src/MessageInput.js
@@ -1,12 +1,16 @@
 import React from "react"
 import thanos from './thanos.gif'
 
+import './MessageInput.css'
+
 export default class MessageInput extends React.Component{
     render(){
         return (
             <div>
-                <img src={thanos} className="App-logo" alt="logo" />
-                <h5>Enter your message here</h5> 
+                <div>
+                    <img src={thanos} className="App-logo" alt="logo"/>
+                </div>
+                <input type="text" display-type="block" placeholder="Your Message"/>
             </div>
         )
     }
