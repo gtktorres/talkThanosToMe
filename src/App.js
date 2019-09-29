898b846a0847344339a3fd9f1c6456bb3d4f26de
Joshi <joshi@172-29-211-89.dynapool.nyu.edu>
2019-09-29 02:11:00 -0400

    Add message thanos functionaliy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import MessageInput from './MessageInput';
-import Background from './Background'
+import React from "react";
+import thanos from "./thanos.gif";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Background from "./Background";
+import Messages from "./Messages";
 
-import './App.css';
+import "./App.css";
 
 export default class App extends React.Component {
-  
-  constructor(){
+  constructor() {
     super();
     this.state = {
       name: "Alfonso",
       food: ["pasta", "pizza", "potatoes"]
-    }
+    };
   }
-  
-  render(){
+
+  render() {
     return (
       <div className="App">
         <Background />
-        <header className="App-header">
-          <h1>Talk Thanos To Me</h1>
+        <div className="App-header">
+          <h1>Talk to Thanos!</h1>
           <h4>Text 1-718-400-6004 to speak to Thanos</h4>
-          <MessageInput />
-    
-        </header>
-        
+          <br />
+          <img src={thanos} className="App-logo" alt="logo" />
+          <Messages />
+        </div>
       </div>
     );
   }
---
+++ src/Messages.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import "./MessageInput.css";
+import axios from "axios";
+
+class Messages extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      userMessage: "",
+      thanosMessage: ""
+    };
+  }
+  //   handleSubmitMessage = e => {
+  //     const messages = [...this.state.messages];
+  //     messages.push(e.target.value);
+  //     this.setState({ messages });
+  //   };
+
+  handleUserMessage = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    this.scrollToBottom();
+  };
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  };
+
+  updateMessages = e => {
+    axios.get("http://localhost:4000/api/message").then(res => {
+      const thanosMessage = res.data.line;
+      this.setState({ thanosMessage });
+    });
+    const messages = [...this.state.messages];
+    const userLine = {
+      sender: "user",
+      text: this.state.userMessage
+    };
+    const thanosLine = {
+      sender: "thanos",
+      text: this.state.thanosMessage
+    };
+    messages.push(userLine);
+    messages.push(thanosLine);
+    const userMessage = "";
+    const thanosMessage = "";
+    this.setState({ messages, userMessage, thanosMessage });
+    this.scrollToBottom();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="messagesContainer">
+          {this.state.messages.map(message => (
+            <div className={message.sender}>{message.text}</div>
+          ))}
+          <div
+            style={{ float: "left", clear: "both" }}
+            ref={el => {
+              this.messagesEnd = el;
+            }}
+          ></div>
+        </div>
+        {/* <form onSubmit={this.handleSubmitMessage}> */}
+        <input
+          type="text"
+          display-type="block"
+          placeholder="Or enter your message here!"
+          onChange={this.handleUserMessage}
+          value={this.state.userMessage}
+          name="userMessage"
+        />
+        <button onClick={this.updateMessages}>Send</button>
+        {/* </form> */}
+      </div>
+    );
+  }
+}
+
+export default Messages;
